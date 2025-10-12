7bc4e6f0f9f53f0c6c841ef826e31785fb333e77
Chithra Anil <chithralekha.anil004@gmail.com>
2025-10-12 22:36:15 +0530

    resolve: named export and showpassword variable

--- web-app/src/components/Login.tsx
+++ web-app/src/components/Login.tsx
@@ -5,25 +5,22 @@ import showPassword from '../assets/showPassword.svg';
 import hidePassword from '../assets/hidePassword.svg';
 import TextField from './TextField';
 
-const Login = () => {
+export const Login = () => {
   const [isShowPassword, setIsShowPassword] = useState(false);
   const [isAdminLogin, setIsAdminLogin] = useState(false);
+  const passwordFieldType = isShowPassword ? 'text' : 'password';
 
   return (
     <div className="flex min-h-screen justify-center items-center">
       <div className="flex flex-col gap-4 items-center">
-        <div className="text-3xl text-[#155f82] font-bold pb-2">
-          Sign in
-        </div>
+        <div className="text-3xl text-[#155f82] font-bold pb-2">Sign in</div>
 
         <form className="flex flex-col gap-2 min-w-[300px]">
-          {!isAdminLogin && (
-            <TextField label="Email" type="text" leftIcon={emailIcon} />
-          )}
+          {!isAdminLogin && <TextField label="Email" type="text" leftIcon={emailIcon} />}
 
           <TextField
             label="Password"
-            type={isShowPassword ? 'text' : 'password'}
+            type={passwordFieldType}
             leftIcon={passwordIcon}
             rightIcon={isShowPassword ? hidePassword : showPassword}
             rightIconOnClick={() => setIsShowPassword(!isShowPassword)}
@@ -36,10 +33,7 @@ const Login = () => {
               </span>
             )}
 
-            <button
-              className="text-[#fff] bg-[#155f82] w-full py-2 rounded-lg"
-              type="submit"
-            >
+            <button className="text-[#fff] bg-[#155f82] w-full py-2 rounded-lg" type="submit">
               {isAdminLogin ? 'Login as Admin' : 'Login'}
             </button>
 
@@ -47,9 +41,7 @@ const Login = () => {
               className="text-xs text-[#6358dc] text-center mt-3 cursor-pointer"
               onClick={() => setIsAdminLogin(!isAdminLogin)}
             >
-              {isAdminLogin
-                ? 'Switch to User Login'
-                : 'Switch to Admin Login'}
+              {isAdminLogin ? 'Switch to User Login' : 'Switch to Admin Login'}
             </span>
           </div>
         </form>
@@ -57,5 +49,3 @@ const Login = () => {
     </div>
   );
 };
-
-export default Login;
