99577d96bfd715523ed019ed9a4ed9b7d53526c6
Chithra Anil <chithralekha.anil004@gmail.com>
2025-10-17 23:44:17 +0530

    feat: tanstackquery provider

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './app';
+import { QueryClientProvider, useQueryClient } from '@tanstack/react-query';
+
+const queryClient = useQueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
