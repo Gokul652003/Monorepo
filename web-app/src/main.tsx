409628bd871f85baf009bd61d6650fa2e13eb264
Chithra Anil <chithralekha.anil004@gmail.com>
2025-10-18 12:56:11 +0530

    fix: inproper creation of queryclient

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './app';
-import { QueryClientProvider, useQueryClient } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = useQueryClient();
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
