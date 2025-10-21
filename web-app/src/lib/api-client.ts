477281e605aee1a34ac9f5bd36875dfd4b9c6a2f
Chithra Anil <chithralekha.anil004@gmail.com>
2025-10-21 14:42:55 +0530

    fix:access_token from  getSession, return response and unauthorized status code correction

--- web-app/src/lib/api-client.ts
+++ web-app/src/lib/api-client.ts
@@ -1,37 +1,39 @@
-import paths from "@/config/paths";
-import axios from "axios";
+import paths from '@/config/paths';
+import axios from 'axios';
+import { supabase } from './supabase-client';
 
-const BASE_URL = import.meta.env.VITE_API_URL ;
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
   },
 });
 
 // Request interceptor
 api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("access_token");
+  async (config) => {
+    const session = await supabase.auth.getSession();
+    const token = session.data.session?.access_token;
     if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
-// Response interceptor 
+// Response interceptor
 api.interceptors.response.use(
-  (response) => response.data,
+  (response) => response,
   (error) => {
-    if(error.response?.status === 403){
-      window.location.href = paths.auth
+    if (error.response?.status === 401) {
+      window.location.href = paths.auth;
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export default api;
